56a397ce3c00532bd1aca816aea080bf2cba6a5c
matt-codesstuff <mattmatt.c0d35@gmail.com>
2023-02-27 13:47:06 +0100

    set up admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Guest, Booking, Room
+
+admin.site.register(Guest)
+admin.site.register(Booking)
+admin.site.register(Room)
--- bookings/models.py
+++ bookings/models.py
@@ -11,17 +11,26 @@ class Guest(models.Model):
         return f'{self.first_name} {self.last_name}'
     
 
-class Booking(models.Model):
-    main_guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
-    total_guests = models.IntegerField()
-    rate = models.DecimalField(max_digits=6, decimal_places=2)
-    check_in = models.DateTimeField('check-in')
-    check_out = models.DateTimeField('check-out')
-
-
 class Room(models.Model):
     identifier = models.CharField(max_length=8)
     occupancy = models.IntegerField()
 
     def __str__(self):
-        return self.identifier        
+        return self.identifier    
+    
+
+class Booking(models.Model):
+    main_guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, blank=True, default=None)
+    total_guests = models.IntegerField()
+    rate = models.DecimalField(max_digits=6, decimal_places=2)
+    check_in = models.DateTimeField('check-in')
+    check_out = models.DateTimeField('check-out')
+
+    def __str__(self):
+        ci = self.check_in.strftime('%d/%m/%Y')
+        co = self.check_out.strftime('%d/%m/%Y')
+        return f'{self.main_guest}, {ci} - {co}'
+
+
+    
