5c8ecc20360f4398498ae4c515429149548633ae
matt-codesstuff <mattmatt.c0d35@gmail.com>
2023-02-27 09:59:00 +0100

    added 2 more views

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:booking_id>', views.detail, name='detail'),
+    path('new', views.new_booking, name='new_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello world')
+
+def detail(request, booking_id):
+    return HttpResponse(f'you are viewing detalis for {booking_id} booking')
+
+def new_booking(request):
+    return HttpResponse('You are creating a new booking')
