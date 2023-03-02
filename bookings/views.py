98803b711939a4f7c3bc60ec457358d9708efb5b
matt-codesstuff <mattmatt.c0d35@gmail.com>
2023-03-02 12:00:49 +0100

    [playing around with displaying dates in the template

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
@@ -7,16 +9,23 @@ from .models import Booking
 
 def index(request, *date_check):
     booking_list = []
+    dates = []
     bookings = Booking.objects.all()
 
     if not date_check:
-        date_check = timezone.now()
+        date_check = timezone.now()        
 
     for booking in bookings:
         if  booking.check_in <= date_check and date_check <= booking.check_out:
-            booking_list.append(booking)  
+            booking_list.append(booking) 
 
-    return render(request, 'bookings/index.html', {'booking_list': booking_list})
+    for i in range(10):
+        dates.append(date_check.strftime('%d/%m/%y'))
+        date_check += timedelta(days=1)         
+
+    return render(request, 'bookings/index.html', 
+                  {'booking_list': booking_list, 
+                   'dates' : dates})
 
 def detail(request, booking_id):
     return HttpResponse(f'you are viewing detalis for {booking_id} booking')
