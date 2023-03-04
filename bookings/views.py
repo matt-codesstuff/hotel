157784b01c9bbf476d9e761fb84340c8e89663e9
matt-codesstuff <mattmatt.c0d35@gmail.com>
2023-03-04 14:10:08 +0100

    minor changes to views, index.html and style.css

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,3 @@
-import copy
-
 from datetime import timedelta
 
 from django.shortcuts import render
@@ -22,24 +20,25 @@ def index(request, *date_check):
     bookings = Booking.objects.filter(check_in__range=(date_check - timedelta(days=14), date_check + timedelta(days=14)))    
 
     date_check_header = date_check        
-
+    # prepare a list of dates
     for i in range(DAYCOUNT):
         dates.append(date_check_header.strftime('%d/%m/%y'))
         date_check_header += timedelta(days=1)
-
+    # populate booking_list with a new list for each room
     for room_nr in rooms:        
         booking_list.append([room_nr])
-        bookings_per_room = bookings.filter(room=room_nr)
-
+    # populate each day of room list
     for room_row in booking_list:
         for i in range(DAYCOUNT):
             room_row.append(None)
 
     for row in booking_list:
+        # narrow lookup to only bookings for the current room
         bookings_per_room = bookings.filter(room=row[0])
         if bookings_per_room:
             for day in row:
                 for booking in bookings_per_room:
+                    # reset this variable for each booking
                     date_check_booking = date_check
                     for i , j in enumerate(range(DAYCOUNT), 1):
                         if booking.check_in <= date_check_booking <=booking.check_out:
